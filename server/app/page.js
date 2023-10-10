74794f293247c9c453e591202e8571c60497fc4f
sangs1994 <sangeetha94.chandrasekar@gmail.com>
2023-10-10 14:43:03 +0100

    Updates

--- server/app/page.js
+++ server/app/page.js
@@ -333,14 +333,14 @@ const routeModule = new AppPageRouteModule({
 
 /***/ }),
 
-/***/ 9489:
+/***/ 5724:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1232, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6926, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2987, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 4282, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 831, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6926, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 4282, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6505, 23))
 
 /***/ }),
@@ -715,9 +715,8 @@ function MySkills() {
         "Node JS",
         "Express",
         "Next JS",
-        "Tailwind CSS",
         "REST API",
-        "Mysql"
+        "Mysql Queries"
     ];
     return /*#__PURE__*/ jsx_runtime_.jsx("div", {
         className: "flex gap-4 flex-wrap color-text-blue ",

3716334983096e0ffb80f27e292169b4738ca816
sangsthedeveloper <sangeetha94.chandrasekar@gmail.com>
2025-04-21 19:22:36 +0100

    Removing Projects
    Removing Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,48 +2,13 @@ import CardProject from "./CardProject";
 
 const datas = [
   {
-    projectName: "Pharmacy Web App",
-    description:
-      "Pharmacy App is an e-commerce web application for selling products and medicines",
-    image: "/pharmacy.png",
-    link: "https://pharmacy-app.yuandaputri.com/",
-    tags: [
-      "NodeJS",
-      "ReactJS",
-      "Express",
-      "Redux",
-      "Formik",
-      "JWT",
-      "Nodemailer",
-      "Core Java"
-    ],
-  },
-  {
     projectName: "Portfolio App",
     description:
       "A simple portfolio application with email service, skills and google maps to redirect my location..",
     image: "/portfolio.png",
     link: "https://sangs1994.github.io/Portfolio/",
-    tags: [
-      "HTML",
-      "CSS",
-      "Javascript"
-    ],
+    tags: ["HTML", "CSS", "Javascript"],
   },
-  {
-    projectName: "To Do List",
-    description: "A todolist app, as a user, you can create list of your activity that you trying to do. You also can set the deadline time. A notification by email is sent to you once you meet the deadline.",
-    image: "/kanban.png",
-    link: "https://mythreads.yuandaputri.com/",
-    tags: [
-      "NextJS",
-      "Typescript",
-      "MongoDB",
-      "Clerk",
-      "Uploadthing",
-      "Tailwind",
-    ],
-  }
 ];
 
 function Projects() {
